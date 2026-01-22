d291de176c34d74807a79d08d906045cffe189be
Sagar Chavda <sagarchavda3571@gmail.com>
2026-01-22 22:59:17 +0530

    code change

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-  domains: ['res.cloudinary.com'],
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "**",
-    },
-  ],
-}
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
