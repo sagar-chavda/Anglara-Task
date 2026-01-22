008131e425d1740c5c1544e2450c8e5251beb8a6
Sagar Chavda <sagarchavda3571@gmail.com>
2026-01-22 22:39:59 +0530

    config.ts file change

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
