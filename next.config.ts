21ff4d24c01ecf3de842beac42a9f55946550fde
Sagar Chavda <sagarchavda3571@gmail.com>
2026-01-22 22:46:38 +0530

    config.ts file changes.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,14 @@
-import type { NextConfig } from "next";
-
-// Source - https://stackoverflow.com/a
-// Posted by li Anna
-// Retrieved 2026-01-22, License - CC BY-SA 4.0
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'm.media-amazon.com',
-                pathname: '**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '**'
-            }
-        ]
-    }
+  images: {
+  domains: ['res.cloudinary.com'],
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**",
+    },
+  ],
+}
 };
 
 module.exports = nextConfig;
